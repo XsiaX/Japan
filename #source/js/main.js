1800b0b9bc23527c96eae0696bb3d4787833b098
Anastasia But <anastasia27but@gmail.com>
2022-02-19 12:13:06 +0200

    Add adaptive vesion of website

--- #source/js/main.js
+++ #source/js/main.js
@@ -1,10 +1,80 @@
 (function() {
+  const video = document.querySelector(".videos");
+  let videosLine = video.offsetTop;
   const header=document.querySelector(".header");
+  const headerElHeight =  document.querySelector('.header').clientHeight;
   window.onscroll=()=>{
-    if (window.pageYOffset>50) {
+    if (window.pageYOffset>50 && window.pageYOffset<=videosLine - 5) {
+      document.getElementsByClassName('header')[0].style.opacity="1";
+      header.classList.add("header");
       header.classList.add("header_active");
-    } else {
+    } 
+    else if (window.pageYOffset<=50 ) {
+      document.getElementsByClassName('header')[0].style.opacity="1";
+      header.classList.add("header");
       header.classList.remove("header_active");
+
+    }
+    else {
+      document.getElementsByClassName('header')[0].style.opacity="0";
+      header.classList.remove("header_active");
+
     }
   };
+}());
+
+//Burger handler
+(function() {
+const burgerItem = document.querySelector('.burger');
+const menu = document.querySelector('.header__nav');
+const menuCloseItem = document.querySelector('.header__nav-close');
+burgerItem.addEventListener('click', () => {
+  menu.classList.add('header__nav_active');
+  document.querySelector("body").style.overflow="hidden";
+});
+menuCloseItem.addEventListener('click', () => {
+  menu.classList.remove('header__nav_active');
+  document.querySelector("body").style.overflow="auto";
+
+})
+}());
+
+// Scroll to anchors
+(function () {
+
+  const smoothScroll = function (targetEl, duration) {
+      const headerElHeight =  document.querySelector('.header').clientHeight;
+      let target = document.querySelector(targetEl);
+      let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+      let startPosition = window.pageYOffset;
+      let startTime = null;
+  
+      const ease = function(t,b,c,d) {
+          t /= d / 2;
+          if (t < 1) return c / 2 * t * t + b;
+          t--;
+          return -c / 2 * (t * (t - 2) - 1) + b;
+      };
+  
+      const animation = function(currentTime){
+          if (startTime === null) startTime = currentTime;
+          const timeElapsed = currentTime - startTime;
+          const run = ease(timeElapsed, startPosition, targetPosition, duration);
+          window.scrollTo(0,run);
+          if (timeElapsed < duration) requestAnimationFrame(animation);
+      };
+      requestAnimationFrame(animation);
+
+  };
+
+  const scrollTo = function () {
+      const links = document.querySelectorAll('.js-scroll');
+      links.forEach(each => {
+          each.addEventListener('click', function () {
+              const currentTarget = this.getAttribute('href');
+              smoothScroll(currentTarget, 1000);
+          });
+      });
+  };
+  scrollTo();
 }());
